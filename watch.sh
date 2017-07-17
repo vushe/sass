4503f3ca7e4f80389defdcffb1a18c0bf9bc729d
pablofelix <pablofelix@gmail.com>
2017-07-18 00:24:31 +0200

    watch

--- watch.sh
+++ watch.sh
@@ -5,18 +5,15 @@ if [ "$ENV" = 'watch' ]; then
 		echo "Running watch script for sass"
 		MONITORDIR="/app/"
 		echo "${MONITORDIR}"
-	inotifywait -m -r -e modify,close_write,move,create,delete --format '%w%f' "/app/" | while read FILE_CHANGED 
+	inotifywait -m -r -e modify,close_write,move,create,delete --format '%w%f' "/app/" | while read FILE_CHANGED
 
 	do
 
-
-					exec sass /app/style.scss /app/css/style.css --style compressed
-
-			echo "style.css modificado"
+			exec sass /app/style.scss /app/css/style.css --style compressed
 
 
 	done
 else
-	exec sass -h
-	echo "do nothing with sass"
+	exec sass
+
 fi
