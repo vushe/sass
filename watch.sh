69ac979a1f2b489cc8a87b81bedff6b2820b98b4
pablofelix <pablofelix@gmail.com>
2017-08-01 16:18:02 +0200

    watch script updated

--- watch.sh
+++ watch.sh
@@ -3,18 +3,27 @@ set -e
 #function watcher with inotify
 if [ $@ = 'watch' ]; then
 		echo "Running watch script for sass"
-		if [  $( $( ls -d /app/*/ 2>/dev/null) | grep scss | wc -l) = 0 ]; then
-			cd /app/ && mkdir scss && cd scss && touch style.scss
+		if [  $( ls -d /app/*/  | grep sass | wc -l) 2>/dev/null = 0 ]; then
+			echo "/sass/style.scss was created"
+			cd /app/ && mkdir sass && cd sass && touch style.scss && chmod 751 -R /app/sass
+		else	
+			echo "styles.scss was detected in sass directory"
 		fi
-		if [ $( $( ls  /app/style.scss 2>/dev/null) | grep scss | wc -l) = 0 ];then
-			touch style.scss
+		if [ $(  ls  /app/sass/style.scss  | grep /app/sass/style.scss | wc -l) 2>/dev/null = 0 ];then
+			echo "style.scss was created"
+			cd /app/sass/ && touch style.scss && chmod 751 /app/sass/style.scss
+		else
+			echo "styles.scss was detected in sass directory"
 		fi
-		if [ $( $(ls  -d /app/*/ 2>/dev/null) | grep css | wc -l) = 0 ]; then
-			cd /app/ && mkdir css && cd css && touch style.css
+		if [  $(ls  -d /app/*/ l | grep css | wc -l) 2>/dev/null = 0 ]; then
+			echo "css directory was created"
+			cd /app/ && mkdir css && chmod 751 -R /app/css
+		else
+			echo "css directory was detected"
 		fi
                 
 		MONITORDIR="/app/"
-		inotifywait -m -r -e modify,close_write,move,create,delete --format '%w%f' "/app/scss/" | while read FILE_CHANGED
+		inotifywait -m -r -e modify,close_write,move,create,delete --format '%w%f' "/app/sass/" | while read FILE_CHANGED
 
 	do
 			exec sass /app/sass/style.scss /app/css/style.css --style compressed
